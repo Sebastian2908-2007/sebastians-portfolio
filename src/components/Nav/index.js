44015de0d00d0c86db9f999ed124a5af4c75b097
Sebabin <sebastiancrespin279@gmail.com>
2022-04-14 15:39:51 -0600

    nav is pretty much styled but has all of the functionality it needs

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,28 +1,76 @@
+import { useState } from "react";
+
+
+
 function Nav(props) {
-    const { currentPage,setCurrentPage } = props
-    console.log(currentPage);
+    // state for displaying the right componenet]
+    const { currentPage,setCurrentPage } = props;
+    // state for about link
+    const [isAboutActive,setIsAboutActive] = useState(false);
+    // state for portfolio link
+    const [isPortfolioActive,setIsPortfolioActive] = useState(false);
+    // state for resume link
+    const [isResumeActive,setIsResumeActive] = useState(false);
+    // state for Contact link
+    const [isContactActive,setIsContactActive] = useState(false);
+
+    // functions below set state true for referenced section as well as setting false state for all the others probably not the most elegant solution but it works for now
+    const setAllFalseAbout = () => {
+        setIsAboutActive(true);
+        setIsPortfolioActive(false);
+        setIsResumeActive(false);
+        setIsContactActive(false);
+    }
+    const setAllFalsePortfolio = () => {
+        setIsAboutActive(false);
+        setIsPortfolioActive(true);
+        setIsResumeActive(false);
+        setIsContactActive(false);
+    }
+    const setAllFalseResume = () => {
+        setIsAboutActive(false);
+        setIsPortfolioActive(false);
+        setIsResumeActive(true);
+        setIsContactActive(false);
+    }
+    const setAllFalseContact = () => {
+        setIsAboutActive(false);
+        setIsPortfolioActive(false);
+        setIsResumeActive(false);
+        setIsContactActive(true);
+    }
+  
     return (
         <header>
             <h2>Sebastian Bowen</h2>
-            <nav>
-                <ul>
-                    <li>
-                        <span onClick={() => setCurrentPage('about')}>
+            <nav className="container">
+                <ul className="list-unstyled row">
+                    <li className={`col-3 ${isAboutActive ? "navActive": null}`}
+                    onClick={setAllFalseAbout}
+                    
+                    >
+                        <span className="text-wrap fs-6" onClick={() => setCurrentPage('about')}>
                             About
                         </span>
                     </li>
-                    <li>
-                        <span onClick={() => setCurrentPage('portfolio')}>
+                    <li className={`col-3 ${isPortfolioActive ? "navActive": null}`}
+                    onClick={setAllFalsePortfolio}
+                    >
+                        <span className="text-wrap fs-6" onClick={() => setCurrentPage('portfolio')}>
                           Portfolio
                         </span>
                     </li>
-                    <li>
-                        <span onClick={() => setCurrentPage('resume')}>
+                    <li className={`col-3 ${isResumeActive ? "navActive": null}`}
+                    onClick={setAllFalseResume}
+                    >
+                        <span className="text-wrap fs-6" onClick={() => setCurrentPage('resume')}>
                            Resume
                         </span>
                     </li>
-                    <li>
-                        <span onClick={() => setCurrentPage('contact')}>
+                    <li className={`col-3 ${isContactActive ? "navActive": null}`}
+                    onClick={setAllFalseContact}
+                    >
+                        <span className="text-wrap fs-6" onClick={() => setCurrentPage('contact')}>
                            Contact
                         </span>
                     </li>
