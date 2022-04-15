82b9c9a6e3e4f31d47cce109fdbf08ab122f7bd8
Sebabin <sebastiancrespin279@gmail.com>
2022-04-15 11:29:33 -0600

    have resume section set up with my bootcamp badge as well as including the ability to download my resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,16 @@
 function Resume() {
     return(
-        <div>
+        <div className='d-flex justify-content-center'>
+        <div className='d-flex flex-column justify-content-center justify-content-evenly w-75 min-vh-100'>
+            <a className='d-flex justify-content-center' target="_blank" rel="noreferrer" alt="my badge" href="https://www.credly.com/badges/29dff5f7-ab06-4ab6-81bc-977bb47070af/public_url">Badge Details</a> 
+            <div className='d-flex justify-content-center'>
+            <img  height="281" width="281" alt="sebastians badge" src={require('../../assets/images/bootcamp-badge.png')} />
+            </div> 
             <a href={require("../../assets/resume/Sebastians-Resume.pdf")} download='Sebastians Resume'>
-            <p>Click to download my resume</p>
+            <p className='d-flex justify-content-center' >Click to download my resume</p>
             </a>
-            <img alt="sebastians badge" src={require('../../assets/images/bootcamp-badge.png')} />
-          <a target="_blank" rel="noreferrer" alt="my badge" href="https://www.credly.com/badges/29dff5f7-ab06-4ab6-81bc-977bb47070af/public_url">Badge Details</a>  
+          
+        </div>
         </div>
     );
 };
