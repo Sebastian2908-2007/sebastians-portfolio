f6da6d1a0526da8425076a62c00686f89365e7e0
Sebabin <sebastiancrespin279@gmail.com>
2022-04-15 01:19:39 -0600

    have my badge added to the resume page as well as my resume being able to be downloaded from the resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,6 +1,12 @@
 function Resume() {
     return(
-        <div>I'm Resume</div>
+        <div>
+            <a href={require("../../assets/resume/Sebastians-Resume.pdf")} download='Sebastians Resume'>
+            <p>Click to download my resume</p>
+            </a>
+            <img alt="sebastians badge" src={require('../../assets/images/bootcamp-badge.png')} />
+          <a target="_blank" rel="noreferrer" alt="my badge" href="https://www.credly.com/badges/29dff5f7-ab06-4ab6-81bc-977bb47070af/public_url">Badge Details</a>  
+        </div>
     );
 };
 
