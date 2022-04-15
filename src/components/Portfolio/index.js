8db8030522b460b1db799fbdd1d8e897e993636e
Sebabin <sebastiancrespin279@gmail.com>
2022-04-15 13:43:39 -0600

    have portfolio section set up functioning and looking good

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,60 @@
 function Portfolio() {
     return(
-        <div>I'm Portfolio</div>
+        <section className="d-flex flex-column justify-content-center">
+<div className="card">
+  <img className="card-img-top"   src={require('../../assets/images/1188.png')} alt="1188 RE App"/>
+  <div className="card-body">
+    <h5 className="card-title">1188 Real Estate</h5>
+    <p className="card-text"><a href='https://github.com/Sebastian2908-2007/1188-real-estate' target="_blank" rel='noreferrer'>Github link</a></p>
+    <a href=" https://still-chamber-32033.herokuapp.com" target="_blank" rel='noreferrer' className="btn btn-primary">Deployed Site</a>
+  </div>
+</div>
+
+<div className="card">
+  <img className="card-img-top"   src={require('../../assets/images/budget-tracker.png')} alt="budget tracker app"/>
+  <div className="card-body">
+    <h5 className="card-title">Budget Tracker</h5>
+    <p className="card-text"><a href='https://github.com/Sebastian2908-2007/budget-tracker-two' target="_blank" rel='noreferrer'>Github link</a></p>
+    <a href="https://powerful-refuge-24488.herokuapp.com/" target="_blank" rel='noreferrer' className="btn btn-primary">Deployed Site</a>
+  </div>
+</div>
+
+<div className="card">
+  <img className="card-img-top"   src={require('../../assets/images/food-fest.png')} alt="food festival app"/>
+  <div className="card-body">
+    <h5 className="card-title">Food Festival</h5>
+    <p className="card-text"><a href='https://github.com/Sebastian2908-2007/food-festival-two' target="_blank" rel='noreferrer'>Github link</a></p>
+    <a href="https://sebastian2908-2007.github.io/food-festival-two/" target="_blank" rel='noreferrer' className="btn btn-primary">Deployed Site</a>
+  </div>
+</div>
+
+<div className="card">
+  <img className="card-img-top"   src={require('../../assets/images/lead-gen.png')} alt="lead generation app"/>
+  <div className="card-body">
+    <h5 className="card-title">Lead generation cms</h5>
+    <p className="card-text"><a href='https://github.com/Sebastian2908-2007/lead-generation-cms' target="_blank" rel='noreferrer'>Github link</a></p>
+    <a href="https://protected-falls-55914.herokuapp.com/" target="_blank" rel='noreferrer' className="btn btn-primary">Deployed Site</a>
+  </div>
+</div>
+
+<div className="card">
+  <img className="card-img-top"   src={require('../../assets/images/photo-port.png')} alt="photo port app"/>
+  <div className="card-body">
+    <h5 className="card-title">Photo Port</h5>
+    <p className="card-text"><a href='https://github.com/Sebastian2908-2007/photo-port-two' target="_blank" rel='noreferrer'>Github link</a></p>
+    <a href="https://sebastian2908-2007.github.io/photo-port-two/" target="_blank" rel='noreferrer' className="btn btn-primary">Deployed Site</a>
+  </div>
+</div>
+
+<div className="card">
+  <img className="card-img-top"   src={require('../../assets/images/pizza-hunt.png')} alt="pizza hunt app"/>
+  <div className="card-body">
+    <h5 className="card-title">Pizza Hunt</h5>
+    <p className="card-text"><a href='https://github.com/Sebastian2908-2007/pizza-hunt-3' target="_blank" rel='noreferrer'>Github link</a></p>
+    <a href="https://immense-springs-41012.herokuapp.com/" target="_blank" rel='noreferrer' className="btn btn-primary">Deployed Site</a>
+  </div>
+</div>
+        </section>
     )
 };
 export default Portfolio;
