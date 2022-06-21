53c2293e0f3a1f84cc7d5ab2af4727dabc7b6871
Sebabin <sebastiancrespin279@gmail.com>
2022-06-21 08:30:01 -0600

    fixed typos in about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,10 +5,10 @@ function About() {
         <div className="card" >
                <img src={require('../../assets/about-images/my-interests.png')} className="card-img-top about-img" alt="nice"/>
           <div className="card-body">
-            <p className="card-text">This is why I do what I do the programing the freelancing. 
-            Because there is nothing better than spending a day in 
-            a beautiful place with this company, it doesn't even have to be a beautiful 
-            place with them any place will do.</p>
+            <p className="card-text">
+            This is why I do what I do the programming the freelancing. Because there is nothing better than spending a day in a beautiful place with 
+            this company, it doesn't even have to be a beautiful place with them any place will do. 
+            </p>
           </div>
        </div>  
 
@@ -24,14 +24,14 @@ function About() {
          <div className="card " >
                <img  src={require('../../assets/about-images/camping-interest.png')}  className="card-img-top" alt="nice"/>
           <div className="card-body">
-            <p className="card-text">No electricity out here! Who needs it when I've got her!.</p>
+            <p className="card-text">No electricity out here! Who needs it when I've got this beauty!.</p>
           </div>
        </div>  
        
         <div className="card ">
                <img src={require('../../assets/about-images/interest-family.png')} className="card-img-top" alt="nice"/>
           <div className="card-body">
-            <p className="card-text">No mortal can escape the jaws of time...Spend it with those that matter the most..</p>
+            <p className="card-text">No mortal can escape the jaws of time...Spend it with those that matter the most!.</p>
           </div>
        </div>  
     </div>
